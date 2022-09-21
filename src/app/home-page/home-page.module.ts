3e5e60f442583c9830b0d282dc871d975ac43565
VitaliyNarytnyk <vitaliy.narytnyk@gmail.com>
2022-09-21 13:42:21 +0300

    created card component, added css styles

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { HomePageRoutingModule } from './home-page-routing.module';
+import { CardComponent } from '../shared/components/card/card.component';
 
 
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [
+    HomePageComponent,
+    CardComponent
+  ],
   imports: [
     CommonModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
   ],
   exports: [HomePageComponent]
 })
